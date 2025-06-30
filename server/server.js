8d20c1ad37697b31bc752a7342616e7565762f2c
himanshuu932 <himanshuu932@gmail.com>
2025-07-01 00:40:14 +0530

    orginin true

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const startMedicationScheduler = require('../server/scheduler/medicationSchedule
 const app = express();
 
 // 🔐 CORS setup to allow frontend requests
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // 📦 Middleware
 app.use(express.json());
