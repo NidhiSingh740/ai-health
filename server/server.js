ad6d876a0f02f44ce0dff0d37a77b2ee1d90797d
himanshuu932 <himanshuu932@gmail.com>
2025-07-01 01:35:10 +0530

    polling added

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const startMedicationScheduler = require('../server/scheduler/medicationSchedule
 const app = express();
 
 // 🔐 CORS setup to allow frontend requests
-app.use(cors({ origin: "https://arogyaai.vercel.app", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // 📦 Middleware
 app.use(express.json());
